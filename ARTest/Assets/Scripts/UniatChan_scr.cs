b5df03d6dbef2d374b99e114037dead37c9d1200
Alain <barraganalain55@gmail.com>
2018-03-13 12:41:42 -0600

    Marcas para los enemigos
    marcas para enemigos y multiples

--- ARTest/Assets/Scripts/UniatChan_scr.cs
+++ ARTest/Assets/Scripts/UniatChan_scr.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vuforia;
 
-public class UniatChan_scr : MonoBehaviour
+public class UniatChan_scr : MonoBehaviour, ITrackableEventHandler
 {
+    private TrackableBehaviour trackable;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        trackable = this.GetComponent<TrackableBehaviour>();
+        if (trackable)
+            trackable.RegisterTrackableEventHandler(this);
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status PreviousSta, TrackableBehaviour.Status NewSta)
+    {
+        if (NewSta == TrackableBehaviour.Status.DETECTED || NewSta == TrackableBehaviour.Status.TRACKED)
+            Debug.Log("Ia estoy en la escena haha salu2");
+        else
+            Debug.Log("Ia me voy a dormir");
+    }
 }
